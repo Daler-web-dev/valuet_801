b14e09a56d83b9f4af1d64895c7846c71073aa82
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2022-11-07 19:31:19 +0500

    created page of wallets

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const configg = {
 
 let transes = document.querySelector('.fortranses')
 
-for(let i = 0; i < 7; i++){
+for(let i = 0; i < 10; i++){
     let box = document.createElement('div')
     box.classList.add('box')
 
